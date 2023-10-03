8e4f7e9a54972af6b56b2bf34ef28d36edae79b4
Ritvik Thukral <79583616+ritvikthukral@users.noreply.github.com>
2023-10-04 01:05:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React, { useState } from "react";
+import "./App.css";
+import CarSearch from "./components/CarSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        
+      </header>
+      <main>
+        <CarSearch />
+      </main>
+    </div>
+  );
+}
+
+export default App;
